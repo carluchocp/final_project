41b73d5801bd3d211ecc5eb9c5f63738ce96dd9c
carluchocp <104532247+carluchocp@users.noreply.github.com>
2022-09-14 19:26:54 -0400

    Revert "Feature/user login"

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ScrollToTop from "./component/scrollToTop.jsx";
+import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./pages/home.jsx";
-import { Demo } from "./pages/demo.jsx";
-import { Single } from "./pages/single.jsx";
-import injectContext from "./store/appContext.jsx";
+import { Home } from "./pages/home";
+import { Demo } from "./pages/demo";
+import { Single } from "./pages/single";
+import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar.jsx";
-import { Footer } from "./component/footer.jsx";
+import { Navbar } from "./component/navbar";
+import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
